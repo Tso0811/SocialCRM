50cc1bec79fcf0fa4f13b346323738af8cc258cf
Tso0811 <savagetso0811@gmail.com>
2025-06-11 20:36:12 +0800

    加入賦予使用者 公司帳號權限功能

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_company',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_company',)}),
+    )
